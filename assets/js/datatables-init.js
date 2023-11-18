3f041f943f8a6ddc17412a748ad80c70ef636b29
Antonio Montano <antonio.montano@gmail.com>
2023-11-18 10:49:19 +0100

    Update datatables-init.js

--- assets/js/datatables-init.js
+++ assets/js/datatables-init.js
@@ -64,3 +64,15 @@ document.addEventListener('DOMContentLoaded', function() {
         console.error("jQuery is not loaded");
     }
 });
+
+document.querySelectorAll('a.toggle-vis').forEach((el) => {
+    el.addEventListener('click', function (e) {
+        e.preventDefault();
+ 
+        let columnIdx = e.target.getAttribute('data-column');
+        let column = table.column(columnIdx);
+ 
+        // Toggle the visibility
+        column.visible(!column.visible());
+    });
+});
