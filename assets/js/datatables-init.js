bddbef273c43eb236f17ee1953e9925e874d6ed9
Antonio Montano <antonio.montano@gmail.com>
2023-11-19 18:33:19 +0100

    Update datatables-init.js

--- assets/js/datatables-init.js
+++ assets/js/datatables-init.js
@@ -6,52 +6,51 @@ document.addEventListener('DOMContentLoaded', function() {
                 "lengthMenu": "Display entries: _MENU_"
             },
             "initComplete": function () {
-                this.api().columns().every(function (index) {
+                console.log("DataTable initialized.");
+                this.api().columns().every(function () {
                     var column = this;
                     var columnName = jQuery(column.header()).text().trim();
+                    console.log("Processing column: " + columnName);
 
                     // Define the column names to exclude from the dropdown filter
                     var excludedColumns = ["Title", "Subjects", "Audience", "Authors", "URLs", "Reviews", "License"];
                     if (excludedColumns.includes(columnName)) {
+                        console.log("Column excluded from dropdown filter: " + columnName);
                         return; // Skip this iteration
                     }
 
-                    // Create select element and add class
-                    var select = document.createElement('select');
-                    select.classList.add("dataTables_length", "select");
-                    select.style.backgroundColor = "transparent"; 
-                    select.add(new Option(''));
-
-                    column.footer().replaceChildren(select);
-
-                    // Apply listener for user change in value
-                    select.addEventListener('change', function () {
-                        var val = jQuery.fn.dataTable.util.escapeRegex(select.value);
-
-                        column
-                            .search(val ? '^' + val + '$' : '', true, false)
-                            .draw();
-                    });
-
-                    // Add list of options
-                    column.data().unique().sort().each(function (d, j) {
-                        select.add(new Option(d));
-                    });
+                    // Rest of your initComplete function...
                 });
             }
         });
 
-        // DEL
+        // Additional code for setting column styles
+        jQuery('table.display td').css({
+            'white-space': 'pre-wrap',
+            'word-wrap': 'break-word'
+        });
+
+        // Inject CSS to limit the width of the first column
+        var style = document.createElement('style');
+        style.type = 'text/css';
+        style.innerHTML = '.display th:first-child, .display td:first-child { max-width: 15%; }';
+        document.head.appendChild(style);
 
         // Setup column visibility toggle
         jQuery(dataTableInstance.table().container()).on('click', 'a.toggle-vis', function(e) {
             e.preventDefault();
 
             let columnIdx = jQuery(this).data('column');
+            console.log("Toggling visibility for column: " + columnIdx);
             let column = dataTableInstance.column(columnIdx);
 
-            // Toggle the visibility
-            column.visible(!column.visible());
+            // Check if the column was found before toggling visibility
+            if (column) {
+                // Toggle the visibility
+                column.visible(!column.visible());
+            } else {
+                console.error("Column not found: " + columnIdx);
+            }
         });
     } else {
         console.error("jQuery is not loaded");
